ac6e4954886963f8cbe3c9c8a83fa7a2561e175d
Alexander Senf <asenf@ebi.ac.uk>
2019-10-10 15:03:23 +0100

    Enable index reload in running server

--- shortReadServer.py
+++ shortReadServer.py
@@ -8,28 +8,30 @@
 # Usage: curl 127.0.0.1:8000/map?TTTACG
 # Specify the sequence as query flag "?TTTACG" with URL "map" on port 8000
 # The server will return '-1' to indicate stop or '1' to indicate continue
-# '0' to indicate wait has not been assigned yet
-# (The algorithm to properly assign this vaue is still TODO)
+
+# The Index is defined as a global variable and can be re-loaded via '/index' 
 
 import os
 import falcon
 import numpy as np
 from bwapy import BwaAligner
 
-class SRS:
-    index = '/home/asenf/devel/data/aae_ref_AaegL5.0_chr1.fa.gz'
-    mapfile = 'data.dat'
+index = '/home/asenf/devel/data/aae_ref_AaegL5.0_chr1.fa.gz'
+mapfile = 'data.dat'
 
-    def __init__(self):  
-        print('Loading Index: ' + self.index)
-        #options = '-x ont2d -A 1 -B 0'
-        #self.aligner = BwaAligner(self.index, options=options)
-        self.aligner = BwaAligner(self.index)
-        print('Index Loaded')
-        print('Accessing Mapping File: ' + self.mapfile)
-        size = os.path.getsize(self.mapfile)
+def load_index():
+    global aligner
+    print('Loading Index: ' + index)
+    aligner = BwaAligner(index)
+    print('Index Loaded')
+
+class SRS:
+    def __init__(self):
+        load_index()
+        print('Accessing Mapping File: ' + mapfile)
+        size = os.path.getsize(mapfile)
         print('Mask file is size: ' + str(size))
-        self.fp = np.memmap(self.mapfile, dtype='int8', mode='readonly', shape=(1,size))
+        self.fp = np.memmap(mapfile, dtype='int8', mode='readonly', shape=(1,size))
         print('Map File Accessed')
 
     def on_get(self, req, resp):
@@ -37,7 +39,7 @@ class SRS:
         hits = 0
         hits_on_target = 0
         hits_off_target = 0
-        alignments = self.aligner.align_seq(req.query_string)
+        alignments = aligner.align_seq(req.query_string)
         for aln in alignments:
             val = self.fp[0][aln.pos]
             val_low = np.bitwise_and(val, 0x0f)
@@ -54,5 +56,11 @@ class SRS:
                       'HitsOnTarget':hits_on_target,
                       'HitsOffTarget':hits_off_target}
 
+class Util:
+    def on_get(self, req, resp):
+        load_index()
+
 api = falcon.API()
 api.add_route('/map', SRS())
+api.add_route('/index', Util())
+
